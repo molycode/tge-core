414b2cb8a79effac7c2aefd041897f3d5b54f506
Thomas <moly.codes@gmail.com>
2026-05-19 12:31:55 +0200

    fix: disable exceptions and suppress consequent warnings in MSVC standalone builds

--- cmake/compilers/msvc.cmake
+++ cmake/compilers/msvc.cmake
@@ -1,3 +1,7 @@
-add_compile_options(/wd4100)
+add_compile_options(
+	$<$<COMPILE_LANGUAGE:CXX>:/EHs- /EHc->  # Disable C++ exceptions
+	$<$<COMPILE_LANGUAGE:CXX>:/wd4530>      # Unreferenced local exception handler (consequence of /EHs-)
+	$<$<COMPILE_LANGUAGE:CXX>:/wd4100>      # Unreferenced formal parameter
+)
 
 message(STATUS "MSVC ${CMAKE_CXX_COMPILER_VERSION} compiler flags configured")
